3acff4797a92d0ebc7ed5f66ea8f27dc09ee783d
nishiokakota <nishiokakota7466@ezweb.ne.jp>
2020-01-03 21:51:07 +0900

    //注意事項// 100名山のリスト(first_list_page.dart)作りを行った。 このページに飛ぶためにはひとつ前のページをつくるか、ホーム画面から一時的に とべるようボタンを設置する必要がある。 //作業内容// 100名山の名前の入力完了。 //作業予定// 1/3or4に200、300名山の名前入力予定。

---
+++ lib/src/pages/second_list_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EditPage extends StatefulWidget {
+  static String routeName = '/pages/edit_page';
+  EditPage({Key key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<EditPage> {
+
+  var listItem = ['one', 'two', 'three'];
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('App Name'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+          ListTile(title: Text('')),
+
+        ],
+      ),
+    );
+  }
+}
